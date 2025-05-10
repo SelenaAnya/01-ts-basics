325d24ccf035e59c90ea09bb18440ec6e6e121bc
Anna Kovalenko <91620969+SelenaAnya@users.noreply.github.com>
2025-05-10 17:04:02 +0300

    1703 1005

---
+++ src/task-1.ts
@@ -0,0 +1,15 @@
+// const name = "Alice";
+// const age = 30;
+// const isOnline = true;
+
+// console.log(`Name: ${name}`);
+// console.log(`Age: ${age}`);
+// console.log(`Online: ${isOnline}`);
+
+const name: string = "Alice";
+const age: number = 30;
+const isOnline: boolean = true;
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Online: ${isOnline}`);
